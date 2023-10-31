1001b68501496abe4c2e061d934c795ebae7699e
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-10-31 04:16:22 +0100

    added example use api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
+import { signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session, status } = useSession()
+  const [x, setX] = useState('')
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    async function f() {
+      if (session && session.accessToken) {
+        setX(session.accessToken)
+        const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+          headers: {
+            'Authorization': `Bearer ${session.accessToken}`
+          }
+        })
+        const data = await response.json()
+        setPlaylists(data.items)
+      }
+    }
+    f()
+  }, [session])
+
+
   return (
     <main className="pl-80"> {/* Add left padding */}
       <h1 className="text-6xl font-bold flex text-center">
         hello world, this is the homepage
-        <Button className="ml-4">Button</Button>
       </h1>
-    </main>
+
+      <Button className="ml-4 "
+        onClick={() =>
+          signOut('spotify', { callbackUrl: "/" }
+          )}>
+        Log out {session?.user?.name}
+      </Button>
+      <h2>
+      </h2>
+      <div className="flex flex-col">
+        {playlists.map((playlist) => (
+          <div key={playlist.id}>{playlist.name}</div>
+        ))}
+      </div>
+    </main >
   )
 }
 
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,12 @@
 import { signIn } from "next-auth/react"
-
+import { Button } from "@/components/ui/button";
 const Login = () => {
     return (
         <div className='w-full h-screen flex items-center justify-center'>
-            <button className="text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg" onClick={() => signIn('spotify', { callbackUrl: "/" })}>Login with spotify</button>
+            <Button className="text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg"
+                onClick={() =>
+                    signIn('spotify', { callbackUrl: "/" })}>Click to Login with spotify
+            </Button>
         </div>
     );
 }
